64903605ed0486d38957c2d764df3467a1663bb5
Jefferson <jeff-oro@hotmail.com>
2018-08-30 20:31:56 +0200

    footer

---
+++ themes/uncode-child/js/principalbar.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+    var menu = document.querySelector(".row-menu>.row-menu-inner");
+    console.log("==============start============================");
+    console.dir(menu);
+    console.log("==============end============================");
+})
