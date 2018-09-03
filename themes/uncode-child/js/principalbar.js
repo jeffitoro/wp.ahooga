c03ea160dd4fe4c45501840c478c58670ea4e40c
Jefferson <jeff-oro@hotmail.com>
2018-09-03 21:51:27 +0200

    validation

--- themes/uncode-child/js/principalbar.js
+++ themes/uncode-child/js/principalbar.js
@@ -1,18 +1,27 @@
 jQuery(document).ready(function () {
-    var menu = document.querySelector(".row-menu>.row-menu-inner");
-    menu.setAttribute("style","padding:18px");
-    var link_lines = document.querySelector(".row-menu>.row-menu-inner>.mmb-container>div");
-    var logo = document.querySelector("#main-logo");
     var li = document.createElement('li');
-    li.id = "button-lines";
-    li.appendChild(link_lines);
-
     var div = document.createElement("div");
-    div.appendChild(logo);
-    div.className = "col-lg-12 main-menu-container middle";
-    div.id = "menu-top";
-    div.innerHTML += "<ul style='color:white' class='menu-burger'><li><a href='https://devahooga.molengeek.com/test-ride'>Test Ride</a></li><li><a href='https://devahooga.molengeek.com/homepages/e-shop'>E-Shop</a></li></ul>";
-    menu.appendChild(div);
-    menu.querySelector('ul').appendChild(li);
-    console.log(div);
+    
+    window.addEventListener("resize", function (evt) {
+        var width = evt.target.outerWidth;
+        if (width > 960 && document.querySelector("#menu-top")==null) {
+            var menu = document.querySelector(".row-menu>.row-menu-inner");
+            var link_lines = document.querySelector(".row-menu>.row-menu-inner>.mmb-container>div");
+            menu.setAttribute("style", "padding:18px");
+            console.log(link_lines);
+            var logo = document.querySelector("#main-logo");
+            li.id = "button-lines";
+            if(link_lines!==null){
+                li.appendChild(link_lines);
+            }
+
+            div.appendChild(logo);
+            div.className = "col-lg-12 main-menu-container middle";
+            div.id = "menu-top";
+            div.innerHTML += "<ul style='color:white' class='menu-burger'><li><a href='https://devahooga.molengeek.com/test-ride'>Test Ride</a></li><li><a href='https://devahooga.molengeek.com/homepages/e-shop'>E-Shop</a></li></ul>";
+            menu.appendChild(div);
+            menu.querySelector('ul').appendChild(li);
+
+        }
+    })
 })
